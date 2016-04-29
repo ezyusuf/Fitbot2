4a46847a9a9076f5a45cc031b08a66556168e54c
Ezaam Yusuf <ezaamyusuf@gmail.com>
2016-04-29 00:34:40 -0400

    changed verify

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    if (req.query['hub.verify_token'] === 'my_message_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
@@ -28,3 +28,5 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+
