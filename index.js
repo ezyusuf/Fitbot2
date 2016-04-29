43ca3cc91dd680c347e04f9c9e7df332e3115ec6
Ezaam Yusuf <ezaamyusuf@gmail.com>
2016-04-29 17:40:50 -0400

    added text before customized plan

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.post('/webhook/', function (req, res) {
         if (event.message && event.message.text) {
             text = event.message.text
             if (text === 'Day' || text === 'Afternoon' || text === 'Night' ) {
+                sendTextMessage(sender, "Here is your customized plan: ")
                 sendGenericMessage(sender)
                 continue
             }
